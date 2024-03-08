99a6598b0d7a5d1305a41a5762fdca4e8d72cef9
valeriogonini <valeriogonini@gmail.com>
2024-03-08 09:27:28 +0100

    add dadi

---
+++ dadi/js/app.js
@@ -0,0 +1,33 @@
+//Gioco dei dadi
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+//Prima di partire a scrivere codice poniamoci qualche domanda:
+//Che ci sia un array da qualche parte?
+//Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+const dadiElement = document.getElementById("dadi")
+
+dadiElement.addEventListener("click", function () {
+    const numberOne = []
+    const numberTwo = []
+
+const randomNumberOne = Math.floor(Math.random() * 6) +1;
+numberOne.push(randomNumberOne)
+
+const randomNumberTwo = Math.floor(Math.random() * 6) +1;
+numberTwo.push(randomNumberTwo)
+
+console.log("Primo numero",+ randomNumberOne, "Secondo numero", + randomNumberTwo);
+
+if (randomNumberOne > randomNumberTwo){
+    console.log("Hai vinto");
+} else{
+    console.log("Hai perso");
+}
+
+
+
+
+
+
+})
